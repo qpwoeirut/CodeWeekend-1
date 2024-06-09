8c11d55352a177476c035b07e1f2e4f2f657de91
qpwoeirut <stanleyzhong8@gmail.com>
2024-06-08 18:13:33 -0700

    Write a more optimal order evaluator.

--- solvers/simulated_annealing.cpp
+++ solvers/simulated_annealing.cpp
@@ -6,114 +6,180 @@
 using namespace std;
 using pii = pair<int, int>;
 
-// TODO: do some compiler tricks to make this a constexpr on a per-test basis
-// and consider getting the inputs at compile time
-const int MAX_N = 3000;
-
 // Parameters to tune
 constexpr const double TEMP_START = 10;
 constexpr const double TEMP_END = 0.001;
-constexpr const double INSERT_CHANCE = 0.25;  // Insert a monster in the order.
-constexpr const double DELETE_CHANCE = 0.25;  // Delete a monster from the order.
-constexpr const double REVERSE_CHANCE = 0.25;  // Reverse a subarray of the order.
-constexpr const double SHIFT_CHANCE = 0.25;  // Swap the locations of two subarrays in the order.
+constexpr const double REVERSE_CHANCE = 0.5;  // Reverse a subarray of the order.
+constexpr const double SHIFT_CHANCE = 0.5;  // Swap the locations of two subarrays in the order.
 constexpr const int MISSING_PENALTY = 10;
 
-static_assert(INSERT_CHANCE + DELETE_CHANCE + REVERSE_CHANCE + SHIFT_CHANCE == 1.0);
+constexpr const int MAX_LEVEL = 25;  // Only tests #36 and #37 are theoretically higher and those are probably not reachable.
+
+static_assert(REVERSE_CHANCE + SHIFT_CHANCE == 1.0);
+
+
+// TODO: do some compiler tricks to make this a constexpr on a per-test basis
+// and consider getting the inputs at compile time
+const int MAX_N = 3000;
 
 Hero hero;
 Game game;
 Monster monster[MAX_N];
 
-int get_moves_between(pii current_position, pii goal_position, int current_speed)
-{
-    return 0; //TODO: fix this thing because its important
-}
-
-const int POINT_THRESHOLD = 50;
-const int INF = 1e9 + 7;
-const double PI = acos(-1);
-
-Rng rng(8);
-
-pii get_maximal_path(int x, int y, int next_monster, vector<int> monster_order, int num_steps_left, int current_gold)
-{
-    //pick 50 random points on the circumference of the circle of the next monster, then check for each point what the total number of steps to get to the monster after will be
-    pair<pii, pii> best_point = {{INF, INF}, {INF, INF}};
-    
-    for (int i = 0; i < POINT_THRESHOLD; i++)
-    {
-        int theta = rng.next_int(360);
-        pii final_point = {monster[monster_order[next_monster]].x + hero.get_range(hero.level) * cos((double)theta * PI/180), monster[monster_order[next_monster]].y + hero.get_range(hero.level) * sin((double)theta * PI/180)};
-        //calculate the number of moves necessary to get to that point and kill the monster
-        int necessary_moves = get_moves_between({x,y}, final_point, hero.get_speed(hero.level)) + (monster[monster_order[next_monster]].hp + hero.get_power(hero.level) - 1)/hero.get_power(hero.level);
-        if (necessary_moves == best_point.first.first)
-        {
-            //check whether the distance to the next point is smaller
-            if (next_monster + 1 == (int)monster_order.size())
-            {
-                continue;
+// TODO: try to consteval this
+int dist[MAX_LEVEL + 1][MAX_N][MAX_N];
+vector<pii> reachable[MAX_LEVEL + 1];
+void calculate_dist() {
+    hero.reset();
+    for (int level = 0; level <= MAX_LEVEL; ++level) {
+        for (int x=0; x<MAX_N; ++x) {
+            for (int y=0; y<MAX_N; ++y) {
+                dist[level][y][x] = MAX_N * 2;
             }
-            int next_distance = get_moves_between(final_point, {monster[monster_order[next_monster + 1]].x, monster[monster_order[next_monster + 1]].y}, hero.get_prosp_speed(monster[next_monster].exp));
-            if (next_distance < best_point.first.second)
-            {
-                best_point = {{necessary_moves, next_distance}, final_point};
+        }
+
+        for (int x=-MAX_N + 1; x < MAX_N; ++x) {
+            for (int y=-MAX_N + 1; y < MAX_N; ++y) {
+                if (x * x + y * y <= hero.get_range(level) * hero.get_range(level)) reachable[level].emplace_back(y, x);
             }
-        } else if (necessary_moves < best_point.first.first)
-        {
-            //replace the best point with this point
-            best_point = {{necessary_moves, (next_monster + 1 == (int)monster_order.size() ? -1: get_moves_between(final_point, {monster[monster_order[next_monster + 1]].x, monster[monster_order[next_monster + 1]].y}, hero.get_prosp_speed(monster[next_monster].exp)))}, final_point};
+        }
+        reachable[level].shrink_to_fit();
+        // Sort by descending distance because why not.
+        sort(reachable[level].begin(), reachable[level].end(), [](const pii& a, const pii& b) {
+            return a.first * a.first + a.second * a.second > b.first * b.first + b.second * b.second;
+        });
+
+        queue<pii> q;
+        q.emplace(0, 0);
+        dist[level][0][0] = 0;
+        while (q.size() > 0) {
+            const pii cur = q.front(); q.pop();
+            
+            for (const pii& d: reachable[level]) {
+                if (dist[level][cur.first + d.first][cur.second + d.second] > dist[level][cur.first][cur.second] + 1) {
+                    dist[level][cur.first + d.first][cur.second + d.second] = dist[level][cur.first][cur.second] + 1;
+                    q.emplace(cur.first + d.first, cur.second + d.second);
+                }
+            }
         }
     }
-    
-    if (num_steps_left < best_point.first.first)
-    {
-        return {current_gold, (int)monster_order.size() - 1 - next_monster};
-    } 
-    
-    num_steps_left -= best_point.first.first;
-    current_gold += monster[next_monster].gold;
-    
-    if (next_monster + 1 == (int)monster_order.size())
-    {
-        return {current_gold, 0};
-    }
-    return get_maximal_path(best_point.second.first, best_point.second.second, next_monster + 1, monster_order, num_steps_left, current_gold);
 }
 
-pii calculate_order_score(const vector<int>& order)
-{
-    return get_maximal_path(game.start_x, game.start_y, 0, order, game.num_turns, 0);
+pii calculate_order_score(const vector<int>& order) {
+    hero.reset();
+
+    int x = game.start_x, y = game.start_y;
+    int turns = 0;
+    int gold = 0;
+    int i = 0;
+    while (i < order.size()) {
+        const int dx = monster[i].x - x, dy = monster[i].y - y;
+        if (dx * dx + dy * dy <= hero.get_range() * hero.get_range()) {
+            turns += (monster[i].hp + hero.get_power() - 1) / hero.get_power();
+            if (turns >= game.num_turns) return pii(gold, order.size() - i);
+
+            gold += monster[i].gold;
+            hero.add_exp(monster[i].exp);
+            ++i;
+            continue;
+        }
+        const pii best = *max_element(reachable[hero.level].begin(), reachable[hero.level].end(), [&i, &order, &x, &y](const pii& a, const pii& b) {
+            // TODO: this doesn't account for leveling up before attacking the next monster(s).
+            for (int j=i; j<order.size(); ++j) {
+                const int dx_a = abs(monster[order[j]].x - (x + a.first));
+                const int dy_a = abs(monster[order[j]].y - (y + a.second));
+                const int dx_b = abs(monster[order[j]].x - (x + b.first));
+                const int dy_b = abs(monster[order[j]].y - (y + b.second));
+                if (dist[hero.level][dy_a][dx_a] != dist[hero.level][dy_b][dx_b]) {
+                    return dist[hero.level][dy_a][dx_a] < dist[hero.level][dy_b][dx_b];
+                }
+            }
+            return false;
+        });
+
+        x += best.first;
+        y += best.second;
+        ++turns;
+
+        if (turns >= game.num_turns) return pii(gold, order.size() - i);
+    }
+    return pii(gold, 0);
 }
 
-vector<Action> recover_moves(const vector<int>& order);
+vector<Action> recover_actions(const vector<int>& order) {
+    hero.reset();
 
-vector<int> mutate(vector<int> order) {
-    if (order.size() < 2) {
-        const int idx = rng.next_int(order.size() + 1);
+    int x = game.start_x, y = game.start_y;
+    vector<Action> actions;
+    int gold = 0;
+    int i = 0;
+    while (i < order.size()) {
+        const int dx = monster[i].x - x, dy = monster[i].y - y;
+        if (dx * dx + dy * dy <= hero.get_range() * hero.get_range()) {
+            for (int p=0; p<monster[i].hp; p += hero.get_power()) actions.emplace_back("attack", i);
+            if (actions.size() >= game.num_turns) {
+                while (actions.size() > game.num_turns) actions.pop_back();
+                return actions;
+            }
 
+            gold += monster[i].gold;
+            hero.add_exp(monster[i].exp);
+            ++i;
+            continue;
+        }
+        const pii best = *max_element(reachable[hero.level].begin(), reachable[hero.level].end(), [&i, &order, &x, &y](const pii& a, const pii& b) {
+            // TODO: this doesn't account for leveling up before attacking the next monster(s).
+            for (int j=i; j<order.size(); ++j) {
+                const int dx_a = abs(monster[order[j]].x - (x + a.first));
+                const int dy_a = abs(monster[order[j]].y - (y + a.second));
+                const int dx_b = abs(monster[order[j]].x - (x + b.first));
+                const int dy_b = abs(monster[order[j]].y - (y + b.second));
+                if (dist[hero.level][dy_a][dx_a] != dist[hero.level][dy_b][dx_b]) {
+                    return dist[hero.level][dy_a][dx_a] < dist[hero.level][dy_b][dx_b];
+                }
+            }
+            return false;
+        });
+
+        x += best.first;
+        y += best.second;
+        actions.emplace_back("move", x, y);
+
+        if (actions.size() >= game.num_turns) return actions;
     }
+    return actions;
+}
 
+Rng rng(8);
+vector<int> mutate(vector<int> order, const int n_used) {  // pass in a copy to mutate
     const double choice = rng.next_double();
-    if (choice < INSERT_CHANCE) {
-        const int idx = rng.next_int(order.size() + 1);
-    } else if (choice < INSERT_CHANCE + DELETE_CHANCE) {
-
-    } else if (choice < INSERT_CHANCE + DELETE_CHANCE + REVERSE_CHANCE) {
-
+    int idx1 = rng.next_int(n_used);
+    int idx2 = rng.next_int(order.size());
+    if (idx1 > idx2) swap(idx1, idx2);
+    if (choice < REVERSE_CHANCE) {
+        for (int i=0; i+i<idx2 - idx1; ++i) {
+            swap(order[idx1 + i], order[idx2 - i]);
+        }
     } else {
-
+        const int len = rng.next_int(min(idx2 - idx1, (int)(order.size()) - idx2)) + 1;
+        for (int i=0; i<len; ++i) {
+            swap(order[idx1 + i], order[idx2 + i]);
+        }
     }
+    return order;
 }
 
 vector<Action> simulated_annealing(int iterations) {
     int best_score = 0;
     vector<int> best_order;
 
-    vector<int> order;
+    vector<int> order(game.num_monsters);
+    for (int i=0; i<order.size(); ++i) order[i] = i;
+
+    int n_used = 1;
     for (int i=0; i<iterations; ++i) {
         // TODO: consider early termination if no new best is found after a while, also multiple attempts + signal handling for killing
-        const vector<int> new_order = mutate(order);
+        const vector<int> new_order = mutate(order, n_used);
 
         const pii new_result = calculate_order_score(new_order);
         const int new_score = new_result.first - new_result.second * MISSING_PENALTY;
@@ -123,16 +189,18 @@ vector<Action> simulated_annealing(int iterations) {
             best_score = new_score;
             best_order = new_order;
             order = new_order;  // TODO: try swapping instead?
+            n_used = new_result.second;
         } else {
             const double progress = (double)(i) / iterations;
             const double temp = TEMP_START * pow(TEMP_END / TEMP_START, progress);
             if (rng.next_double() < exp((new_score - best_score) / temp)) {
                 order = new_order;  // TODO: try swapping instead?
+                n_used = new_result.second;
             }
         }
     }
 
-    return recover_moves(best_order);
+    return recover_actions(best_order);
 }
 
 int main() {
@@ -144,6 +212,8 @@ int main() {
         monster[i].id = i;
         cin >> monster[i];
     }
+
+    calculate_dist();
 
     const vector<Action> answer = simulated_annealing(100000);
     for (const Action& action: answer) {
