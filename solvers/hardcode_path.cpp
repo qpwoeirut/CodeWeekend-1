053615689289d91e2ae02bb23c8a9d0311741be9
qpwoeirut <stanleyzhong8@gmail.com>
2024-06-09 13:29:17 -0700

    Add fatigue calculation.

--- solvers/hardcode_path.cpp
+++ solvers/hardcode_path.cpp
@@ -53,57 +53,72 @@ int calculate_fatigue(const bitset<N>& killed, const int x, const int y) {
     return fatigue;
 }
 
-vector<Action> follow_path() {
+vector<Action> follow_path(const vector<pii>& path) {
     hero.reset();
     bitset<N> killed;
 
     int x = game.start_x, y = game.start_y;
     vector<Action> actions;
-    for (pii p: PATH) {
-        p.first += rng.next_int(-5, 5);
-        p.second += rng.next_int(-5, 5);
-        while (x != p.first || y != p.second) {
-            for (int i=0; i<game.num_monsters; ++i) {
-                if (!killed[i] && monster[i].hp < 10000 && square(monster[i].x - x) + square(monster[i].y - y) <= square(hero.get_range())) {
-                    for (int pw=0; pw<monster[i].hp; pw += hero.get_power()) actions.emplace_back("attack", monster[i].id);
-                    if ((int)actions.size() > game.num_turns) break;
-                    hero.add_exp(monster[i].exp);
-                    killed[i] = true;
+    int gold = 0;
+    int fatigue = 0;
+    int i = 0;
+    while (i < (int)path.size()) {
+        const pii p = path[i];
+        for (int i=0; i<game.num_monsters; ++i) {
+            if (!killed[i] && monster[i].hp < 10000 && square(monster[i].x - x) + square(monster[i].y - y) <= square(hero.get_range())) {
+                const int added_turns = (monster[i].hp + hero.get_power() - 1) / hero.get_power();
+                fatigue += (added_turns - 1) * calculate_fatigue(killed, x, y);
+                for (int p=0; p<added_turns; ++p) actions.emplace_back("attack", monster[i].id);
+                if ((int)actions.size() > game.num_turns) {
+                    break;
                 }
+                gold += 1000 * monster[i].gold / (1000 + fatigue);
+                hero.add_exp(monster[i].exp);
+                killed[i] = true;
+                fatigue += calculate_fatigue(killed, x, y);
+                ++i;
+                continue;
             }
+        }
 
-            double dx = p.first - x, dy = p.second - y;
-            const int spd = x < 125 ? hero.get_speed() : min(hero.get_range(), hero.get_speed());
-            const double mag = sqrt(min(square(spd) / (dx * dx + dy * dy), 1.0));
+        double dx = p.first - x, dy = p.second - y;
+        const int spd = x < 125 ? hero.get_speed() : min(hero.get_range(), hero.get_speed());
+        const double mag = sqrt(min(square(spd) / (dx * dx + dy * dy), 1.0));
 
-            dx *= mag;
-            dy *= mag;
+        dx *= mag;
+        dy *= mag;
 
-            const int nx0 = x + (int)(dx - 1);
-            const int nx1 = x + (int)dx;
-            const int nx2 = x + (int)(dx + 1);
-            const int ny0 = y + (int)(dy - 1);
-            const int ny1 = y + (int)dy;
-            const int ny2 = y + (int)(dy + 1);
+        const int nx0 = x + (int)(dx - 1);
+        const int nx1 = x + (int)dx;
+        const int nx2 = x + (int)(dx + 1);
+        const int ny0 = y + (int)(dy - 1);
+        const int ny1 = y + (int)dy;
+        const int ny2 = y + (int)(dy + 1);
 
-            const vector<pii> options({{nx0, ny0}, {nx0, ny1}, {nx0, ny2}, {nx1, ny0}, {nx1, ny1}, {nx1, ny2}, {nx2, ny0}, {nx2, ny1}, {nx2, ny2}});
-            const pii nxt = *min_element(options.begin(), options.end(), [&p, &x, &y](const pii& a, const pii& b) {
-                bool a_reachable = square(a.first - x) + square(a.second - y) <= square(hero.get_speed());
-                bool b_reachable = square(b.first - x) + square(b.second - y) <= square(hero.get_speed());
-                if (!a_reachable || !b_reachable) return a_reachable > b_reachable;
+        const vector<pii> options({{nx0, ny0}, {nx0, ny1}, {nx0, ny2}, {nx1, ny0}, {nx1, ny1}, {nx1, ny2}, {nx2, ny0}, {nx2, ny1}, {nx2, ny2}});
+        const pii nxt = *min_element(options.begin(), options.end(), [&p, &x, &y](const pii& a, const pii& b) {
+            bool a_reachable = square(a.first - x) + square(a.second - y) <= square(hero.get_speed());
+            bool b_reachable = square(b.first - x) + square(b.second - y) <= square(hero.get_speed());
+            if (!a_reachable || !b_reachable) return a_reachable > b_reachable;
 
-                const int a_rem = square(p.first - a.first) + square(p.second - a.second);
-                const int b_rem = square(p.first - b.first) + square(p.second - b.second);
-                return a_rem < b_rem;
-            });
+            const int a_rem = square(p.first - a.first) + square(p.second - a.second);
+            const int b_rem = square(p.first - b.first) + square(p.second - b.second);
+            return a_rem < b_rem;
+        });
 
-            x = nxt.first;
-            y = nxt.second;
-            actions.emplace_back("move", x, y);
+        x = nxt.first;
+        y = nxt.second;
+        actions.emplace_back("move", x, y);
+        fatigue += calculate_fatigue(killed, x, y);
 
-        if (turns >= game.num_turns) return pii(gold, game.num_monsters - i);
+        while (x == path[i].first && y == path[i].second) ++i;
+
+        if ((int)actions.size() >= game.num_turns) break;
     }
-    return pii(gold, 0);
+    while ((int)actions.size() > game.num_turns) actions.pop_back();
+
+    cerr << "gold: " << gold << endl;
+    return actions;
 }
 
 int main() {
@@ -131,7 +146,7 @@ int main() {
 
     cerr << "Finished reading input." << endl;
 
-    const vector<Action> answer = follow_path();
+    const vector<Action> answer = follow_path(PATH);
     for (const Action& action: answer) {
         cout << action << '\n';
     }
